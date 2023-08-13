1115576382ec7e1c296cbbe42a7b286c1cb96114
sanjheeg <60531063+sanjheeg@users.noreply.github.com>
2023-08-13 16:04:25 -0700

    calculates age successfully

--- introduction.py
+++ introduction.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QGridLayout, QLineEdit, QPushButton, QVBoxLayout, QLabel, QWidget
-
+from datetime import datetime
 
 class AgeCalculator(QWidget):
     def __init__(self):
@@ -9,25 +9,36 @@ class AgeCalculator(QWidget):
         # create widgets
         grid = QGridLayout()
         name = QLabel("name: ")
-        edit_name = QLineEdit()
+        self.edit_name = QLineEdit()
         birth = QLabel("date of birth mm/dd/yyyy: ")
-        edit_birth = QLineEdit()
+        self.edit_birth = QLineEdit()
 
         button = QPushButton("calculate age")
-        output = QLabel("")
+        button.clicked.connect(self.calculate)
+        self.output = QLabel("")
 
+        # add widgets to grid
         grid.addWidget(name, 0, 0)
-        grid.addWidget(edit_name, 0, 1)
+        grid.addWidget(self.edit_name, 0, 1)
         grid.addWidget(birth, 1, 0)
-        grid.addWidget(edit_birth, 1, 1)
+        grid.addWidget(self.edit_birth, 1, 1)
         # button add [2,0] and we want it to span across 1 row and to columns
         grid.addWidget(button, 2, 0, 1, 2)
-        grid.addWidget(output, 3, 0, 1, 2)
+        grid.addWidget(self.output, 3, 0, 1, 2)
 
         self.setLayout(grid)
+
+    def calculate(self):
+        current = datetime.now().year
+        date = self.edit_birth.text()
+        name = self.edit_name.text()
+        birth = datetime.strptime(date, "%m/%d/%Y").date().year
+        age = current - birth
+        self.output.setText(f"you are {age} years old, {name}")
 
 
 app = QApplication(sys.argv)
 calc = AgeCalculator()
 calc.show()
 sys.exit(app.exec())
+
